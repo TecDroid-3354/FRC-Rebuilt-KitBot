44876642c921457e757ce3f69c404030f6eac4a2
Julián B. <a01714453@tec.mx>
2026-01-12 19:02:06 -0600

    Second Push & commit, just basic stuff, I still miss the libraries and the important code

---
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.kt
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.Shooter
+
+import frc.robot.subsystems.ShooterConfig
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+import com
+
+
+object Shooter : SubsystemBase() {
+    val config = SparkMax
+
+
+}
