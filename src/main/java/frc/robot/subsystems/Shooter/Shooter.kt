7544af80bc3eb486856759ebad7514f828cb8a9f
Julián B. <a01714453@tec.mx>
2026-01-12 23:41:06 -0600

    Shooter kt and shooter config have been finished.
    -Julian B
    12/01/2026

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.kt
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.kt
@@ -1,13 +1,41 @@
 package frc.robot.subsystems.Shooter
 
-import frc.robot.subsystems.ShooterConfig
+
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.config.SparkBaseConfig
+import frc.robot.subsystems.shooterConfig
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
-import com
+
+import com.revrobotics.spark.config.SparkMaxConfig
 
 
 object Shooter : SubsystemBase() {
-    val config = SparkMax
+    val sparkConfig = SparkMaxConfig()
+    val config = shooterConfig
+    val motorController= SparkMax(config.motorControllerID, SparkLowLevel.MotorType.kBrushless)
+
+    fun setSpeed(speed: Double){
+        motorController.set(speed)
+    }
+
+    fun stopMotor(){
+        motorController.set(0.0)
+    }
+
+    fun getSpeed(): Double{
+        return motorController.get()
+    }
+
+    init{
+        sparkConfig.inverted(config.inverted)
+        sparkConfig.idleMode(SparkBaseConfig.IdleMode.kBrake)
+        sparkConfig.smartCurrentLimit(config.motorAmpsLimit)
+
+        motorController.configure(sparkConfig)
+    }
+
 
 
 }
--- src/main/java/frc/robot/subsystems/Shooter/ShooterConfig.kt
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConfig.kt
@@ -3,11 +3,15 @@ package frc.robot.subsystems
 
 data class ShooterConfig  (//this class was created to define the basic information that the motor needs.
     var motorControllerID : Int,
-    var motorAmpsLimit : Double,
+    var motorAmpsLimit : Int,
+
+    var inverted : Boolean, // To be honest we have to check if being inverted is CCW or CW
 )
 
 val shooterConfig = ShooterConfig( // this instace was created to define the basic parameters that the motor needs.
     motorControllerID = TODO(),
-    motorAmpsLimit = 40.0,
+    motorAmpsLimit = 40,
+
+    inverted = false,
 
     )
