f95ef1899da940c06b0ca1469618609075b0f5c9
Julián B. <a01714453@tec.mx>
2026-01-14 17:00:11 -0600

    Finished intake + shooter. -Julian B 14/01/2026

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.kt
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.kt
@@ -3,12 +3,14 @@ package frc.robot.subsystems.Shooter
 
 import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.SparkBase
 import com.revrobotics.spark.config.SparkBaseConfig
 import frc.robot.subsystems.shooterConfig
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 
 import com.revrobotics.spark.config.SparkMaxConfig
+import edu.wpi.first.units.Units
 
 
 object Shooter : SubsystemBase() {
@@ -16,24 +18,24 @@ object Shooter : SubsystemBase() {
     val config = shooterConfig
     val motorController= SparkMax(config.motorControllerID, SparkLowLevel.MotorType.kBrushless)
 
-    fun setSpeed(speed: Double){
-        motorController.set(speed)
+    fun setVoltage(volts: Double){
+        motorController.setVoltage(volts)
     }
 
     fun stopMotor(){
-        motorController.set(0.0)
+        motorController.setVoltage(0.0)
     }
-
+/*
     fun getSpeed(): Double{
         return motorController.get()
     }
-
+*/
     init{
         sparkConfig.inverted(config.inverted)
         sparkConfig.idleMode(SparkBaseConfig.IdleMode.kBrake)
-        sparkConfig.smartCurrentLimit(config.motorAmpsLimit)
+        sparkConfig.smartCurrentLimit(config.currentLimit.`in`(Units.Amps).toInt())
 
-        motorController.configure(sparkConfig)
+        motorController.configure(sparkConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters)
     }
 
 
--- src/main/java/frc/robot/subsystems/Shooter/ShooterConfig.kt
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConfig.kt
@@ -1,16 +1,25 @@
 package frc.robot.subsystems
 
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Current
+
 
 data class ShooterConfig  (//this class was created to define the basic information that the motor needs.
-    var motorControllerID : Int,
-    var motorAmpsLimit : Int,
+    var motorControllerID : Int, //main Motor
+
+    var currentLimit : Current,
+
+    var neutralMode : IdleMode,
 
     var inverted : Boolean, // To be honest we have to check if being inverted is CCW or CW
 )
 
 val shooterConfig = ShooterConfig( // this instace was created to define the basic parameters that the motor needs.
-    motorControllerID = TODO(),
-    motorAmpsLimit = 40,
+    motorControllerID = 1,
+    currentLimit = Units.Amps.of(40.0),
+    neutralMode = IdleMode.kBrake,
+
 
     inverted = false,
 
